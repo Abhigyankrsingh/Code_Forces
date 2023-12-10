005dc60c4ee0bef293592b0cf561ead82a802adb
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-12-10 23:56:28 +0530

    Code_Updated

--- TLE2_1.cpp
+++ TLE2_1.cpp
@@ -1,31 +1,14 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-
-
-int fact (int n){
-
-    if(n ==0 || n == 1){
-        return 1;
-    } else {
-        
-        return n * fact(n-1);
-    }
-
-
+long long fact(long long num){ // 3
+	if(num <= 1){
+		return 1;
+	}
+	return num * fact(num - 1); // 5 * 4 * 3 * 2 * 1
 }
-
-
-
-
-int main(){
-
-    int n;
-
-    cin>>n;
-
-    int result = fact(n);
-
-    cout << result;
-
+int main()
+{
+	long long num;
+	cin>>num;
+	cout<<fact(num);
 }
