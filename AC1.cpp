5348f5ae1066734e39b3a8f488a688f5758733e3
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-01-02 12:51:03 +0530

    Code_Upated

--- AC1.cpp
+++ AC1.cpp
@@ -2,3 +2,27 @@
 
 //B. Preparing for the Contest
 
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int n,k;
+
+     cin >>n >>k;
+
+    vector<int>ans;
+
+    for(int i =0 ; i<=k; i++){
+        ans.pb(i);
+    }
+
+    for(int i =n; i >k; i--){
+        ans.pb(i);
+    }
+
+    cout<< ans << "\n";
+}
+
+
