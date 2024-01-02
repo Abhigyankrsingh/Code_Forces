64fbd1ccebc76431b3186f92e7f32a755ddaa50d
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-01-02 12:51:24 +0530

    Code_Updated

--- AC1.cpp
+++ AC1.cpp
@@ -2,27 +2,31 @@
 
 //B. Preparing for the Contest
 
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(){
+int main() {
+    int n, k;
+    cin >> n >> k;
 
-    int n,k;
+    vector<int> ans;
 
-     cin >>n >>k;
-
-    vector<int>ans;
-
-    for(int i =0 ; i<=k; i++){
-        ans.pb(i);
+    for (int i = 0; i <= k; i++) {
+        ans.push_back(i);
     }
 
-    for(int i =n; i >k; i--){
-        ans.pb(i);
+    for (int i = n; i > k; i--) {
+        ans.push_back(i);
     }
 
-    cout<< ans << "\n";
+    for (int num : ans) {
+        cout << num << " ";
+    }
+    cout << "\n";
+
+    return 0;
 }
 
 
